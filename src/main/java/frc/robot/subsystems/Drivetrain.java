c306714a91ac31f4de20ce9b7b0aca1b659a3bd3
edgeflame147 <lillian.y.yao@gmail.com>
2024-07-24 22:48:34 -0700

    added encoders

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -5,6 +5,7 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -15,12 +16,22 @@ public class Drivetrain extends SubsystemBase {
   private final CANSparkMax backLeftMotor;
   private final CANSparkMax backRightMotor;
 
+  private final RelativeEncoder frontLeftEncoder;
+  private final RelativeEncoder frontRightEncoder;
+  private final RelativeEncoder backLeftEncoder;
+  private final RelativeEncoder backRightEncoder;
+
   /** Creates a new Drivetrain. */
   public Drivetrain() {
     frontLeftMotor = new CANSparkMax(0, MotorType.kBrushless);
     frontRightMotor = new CANSparkMax(1, MotorType.kBrushless);
     backLeftMotor = new CANSparkMax(2, MotorType.kBrushless);
     backRightMotor = new CANSparkMax(3, MotorType.kBrushless);
+
+    frontLeftEncoder = frontLeftMotor.getEncoder();
+    frontRightEncoder = frontRightMotor.getEncoder();
+    backLeftEncoder = backLeftMotor.getEncoder();
+    backRightEncoder = backRightMotor.getEncoder();
   }
 
   @Override
@@ -28,3 +39,4 @@ public class Drivetrain extends SubsystemBase {
     // This method will be called once per scheduler run
   }
 }
+
