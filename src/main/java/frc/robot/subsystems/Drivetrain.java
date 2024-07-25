8cd047476b391f3838bf5773b9d83f0b462294a3
edgeflame147 <lillian.y.yao@gmail.com>
2024-07-24 19:14:41 -0700

    added motors

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -4,11 +4,24 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Drivetrain extends SubsystemBase {
+  private final CANSparkMax frontLeftMotor;
+  private final CANSparkMax frontRightMotor;
+  private final CANSparkMax backLeftMotor;
+  private final CANSparkMax backRightMotor;
+
   /** Creates a new Drivetrain. */
-  public Drivetrain() {}
+  public Drivetrain() {
+    frontLeftMotor = new CANSparkMax(0, MotorType.kBrushless);
+    frontRightMotor = new CANSparkMax(1, MotorType.kBrushless);
+    backLeftMotor = new CANSparkMax(2, MotorType.kBrushless);
+    backRightMotor = new CANSparkMax(3, MotorType.kBrushless);
+  }
 
   @Override
   public void periodic() {
